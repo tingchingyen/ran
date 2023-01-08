ce08664ce7fce6fe03d3a841f44d1fd1ac612be8
tingchingyen <49836660+tingchingyen@users.noreply.github.com>
2023-01-08 16:06:54 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from pptx import Presentation
+from glob import glob
+
+st.write(glob('*'))
+
+
 
 # Open an existing PowerPoint presentation
-prs = Presentation("template.pptx")
+# prs = Presentation("template.pptx")
 
 # Get the first slide of the presentation
-slide = prs.slides[0]
+# slide = prs.slides[0]
 
 # Add a text box to the slide
-textbox = slide.shapes.add_textbox(left=50, top=50, width=200, height=100)
+# textbox = slide.shapes.add_textbox(left=50, top=50, width=200, height=100)
 
 # Set the text of the text box
-textbox.text_frame.text = "Hello, world!"
+# textbox.text_frame.text = "Hello, world!"
 
 
-prs.save("presentation.pptx")
+# prs.save("presentation.pptx")
