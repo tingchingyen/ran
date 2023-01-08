96b4590476027568d215b7a91772dc2fd0b2c244
tingchingyen <49836660+tingchingyen@users.noreply.github.com>
2023-01-08 17:41:35 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from pptx_interface import Presentation
+import openpyxl
 
 # Open an existing PowerPoint presentation
-prs = Presentation("template.pptx")
+wb = openpyxl.load_workbook("template.pptx", read_only=False, keep_vba=True)
 
-# Find the first text box on the first slide
-textbox = prs.slides[0].textboxes[0]
+# Get the first slide
+slide = wb.worksheets[0]
+
+# Find the first text box on the slide
+textbox = slide.cell(row=1, column=1).value
 
 # Set the text of the text box
 textbox.text = "test"
 
 # Set the font size of the text box
 textbox.font.size = 12
-textbox.text = "test2"
 
-# Set the font size of the text box
-textbox.font.size = 8
 # Save the modified PowerPoint presentation
-prs.save("tmp.pptx")
+wb.save("tmp.pptx")
+
+
 with open("tmp.pptx", "rb") as file:
     btn = st.download_button(
             label="Download PPT",
